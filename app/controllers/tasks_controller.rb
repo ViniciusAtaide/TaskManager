4e842aacdafcd9c8caad063614c8f27ebe6cc1d9
Vinicius Ataide <viniciusataid@gmail.com>
2012-04-16 01:48:23 -0300

    adding ajax

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,36 +1,49 @@
 class TasksController < ApplicationController
-
   def index
     @tasks = Task.all
-    @task = Task.new
+    @task = Task.new    
   end
-
-  def new
-    @task = Task.new
-    respond_to do |format|
-      format.html
-      format.json { render json: @task}
-    end
-  end
-
   def create
     @tasks = Task.all
-    @task = Task.new(params[:task])
+    @task = Task.new(params[:task])    
+    
     if @task.save
-      redirect_to tasks_url
-    else
-      render :action => 'index'
+      flash[:notice] = "Sucessfully created task"
+      @tasks = Task.all      
+      respond_to do |format|
+        format.html { redirect_to tasks_path }
+        format.js
+      end
+    else    
+      @task.errors.full_messages.each do |msg|
+        flash[:notice] = msg   
+      end         
     end
   end
   
   def done_delete
-    if params[:commit] == 'Mark selected as done'
-      Task.update_all(["done=?", "true"], :id => params[:tasks_id])
-      redirect_to tasks_path
+    @tasks = Task.all
+    @task = Task.new
+
+    if params[:commit] == 'Mark selected as done'      
+      if Task.update_all(["done=?", "true"], :id => params[:tasks_id])      
+        flash[:notice] = "All marked tasks are done"
+        @tasks = Task.all
+        respond_to do |format|
+         format.html { redirect_to tasks_path }
+         format.js
+        end
+      end
     elsif params[:commit] == "Delete selected"
-      Task.delete_all(:id => params[:tasks_id])
-      Task.delete_all(["done=?", "true"])
-      redirect_to tasks_path
+      
+      if Task.delete_all(:id => params[:tasks_id]) && Task.delete_all(["done=?", "true"])      
+        flash[:notice] = "Sucessfully deleted all tasks"
+        @tasks = Task.all
+        respond_to do |format|
+          format.html { redirect_to tasks_path }
+          format.js
+        end
+      end
     end
   end
 
